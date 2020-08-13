4b0f2e55e6d01a1a98abca2ff9c9d5358224db35
pedrinholemes <pedroca21265@gmail.com>
2020-08-13 19:04:42 -0300

    :rocket: Finish web app and configure PWA

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
+import registerServiceWorker from "./serviceWorker";
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
+registerServiceWorker();
